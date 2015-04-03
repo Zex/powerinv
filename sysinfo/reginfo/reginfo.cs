d432f3447082373b22f87aaf840ebff0712f2f89
zex <top_zlynch@yahoo.com>
2015-04-03 17:16:23 +0800

    read reginfo

---
+++ sysinfo/reginfo/reginfo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace sysinfo
+{
+    class reginfo
+    {
+        public void get_items()
+        {
+            foreach (var k in
+                Microsoft.Win32.Registry.LocalMachine.GetSubKeyNames())
+            {
+                Console.WriteLine("SubKey: {0}", k);
+            }
+
+            Console.WriteLine("---------------------------------------");
+
+            Microsoft.Win32.RegistryKey rkey =
+                Microsoft.Win32.Registry.LocalMachine.OpenSubKey("SOFTWARE\\Google");
+
+            if (rkey != null)
+            {
+                foreach (var k in rkey.GetSubKeyNames())
+                {
+                    Console.WriteLine("SubKeyName : {0}", k);
+                }
+
+                Console.WriteLine("---------------------------------------");
+
+                foreach (var k in rkey.GetValueNames())
+                {
+                    Console.WriteLine("{0}: {1}", k, rkey.GetValue(k));
+                }
+
+            }
+        }
+    }      
+
+    class utils
+    {
+        public void dell_mount(string label, string mount_base)
+        {
+            System.Management.Instrumentation.WmiConfigurationAttribute
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        sysinfo.reginfo ri = new sysinfo.reginfo();
+        ri.get_items();
+    }
+}
