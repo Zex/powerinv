70d47db01119c4d1a3493dfad403bb9b9f87b843
zex <top_zlynch@yahoo.com>
2015-06-04 22:42:46 +0800

    fix path

--- part-helper/ps1/mount-customize.ps1
+++ part-helper/ps1/mount-customize.ps1
@@ -1,11 +1,17 @@
 # mount-customize.ps1 - Mount customize partition to diretory
 # Author: Zex <top_zlynch@yahoo.com>
 #
-# powershell -File <path-to-mount-customize.ps1>
+# powershell -ExecutionPolicy Bypass -File <path-to-mount-customize.ps1>
 #---------------------------------------------------------------------------
-. c:\part-helper\common.ps1
+. c:\part-helper\ps1\common.ps1
 
 $disk_nr, $part_nr = create_customize_mount_dp
 
-diskpart /s $customize_premount_dp
-Add-PartitionAccessPath -DiskNumber $disk_nr -PartitionNumber $part_nr -AccessPath $customize_mp  -ErrorAction Ignore 
+echo "disk_nr: $disk_nr"
+echo "part_nr: $part_nr"
+
+if ($disk_nr -ne $null -and $part_nr -ne $null)
+{
+    diskpart /s $customize_premount_dp
+    Add-PartitionAccessPath -DiskNumber $disk_nr -PartitionNumber $part_nr -AccessPath $customize_mp 
+}
