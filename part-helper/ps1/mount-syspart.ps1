f263d42d8f2ac4fef079c78cf23c97c6bb0316ea
zex <top_zlynch@yahoo.com>
2015-06-04 22:42:53 +0800

    fix path

--- part-helper/ps1/mount-syspart.ps1
+++ part-helper/ps1/mount-syspart.ps1
@@ -1,11 +1,17 @@
 # mount-syspart.ps1 - Mount system partition to diretory
 # Author: Zex <top_zlynch@yahoo.com>
 #
-# powershell -File <path-to-mount-syspart.ps1>
+# powershell -ExecutionPolicy Bypass -File <path-to-mount-syspart.ps1>
 #---------------------------------------------------------------------------
-. c:\part-helper\common.ps1
+. c:\part-helper\ps1\common.ps1
 
 $disk_nr, $part_nr = create_syspart_mount_dp
 
-diskpart /s $syspart_premount_dp
-Add-PartitionAccessPath -DiskNumber $disk_nr -PartitionNumber $part_nr -AccessPath $syspart_mp  -ErrorAction Ignore
+echo "disk_nr: $disk_nr"
+echo "part_nr: $part_nr"
+
+if ($disk_nr -ne $null -and $part_nr -ne $null)
+{
+    diskpart /s $syspart_premount_dp
+    Add-PartitionAccessPath -DiskNumber $disk_nr -PartitionNumber $part_nr -AccessPath $syspart_mp
+}
