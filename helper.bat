514ca6519673d5fcfbbad8c81aa7606cdd57a677
zex <top_zlynch@yahoo.com>
2015-06-05 22:35:26 +0800

    remove helper.bat

--- helper.bat
+++
@@ -1,9 +0,0 @@
-# helper.bat - Create partition from system partition
-# Author: Zex <top_zlych@yahoo.com>
-# 
-# Run as administrator
-# > powershell -File <path-to-helper.ps1>
-
-powershell -File c:\part-helper\mount-customize.ps1
-powershell -File c:\part-helper\mount-syspart.ps1
-
