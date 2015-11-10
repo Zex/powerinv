6a19dcacbc70d54d35c54218bd0504417330f9a6
Zex Li <top_zlynch@yahoo.com>
2015-11-10 23:32:34 +0800

    BACKUP, add fw

---
+++ wm/common/fw/singleton.cs
@@ -0,0 +1,58 @@
+﻿/** fw - singleton
+ * Author: Zex <top_zlynch AT yahoo.com>
+ * 
+ */
+using System;
+
+namespace fw
+{
+    public class SingletonBase
+    {
+        private static SingletonBase _instance;
+        private static readonly object _mutex = new object();
+
+        protected SingletonBase()
+        {
+        }
+
+        public static SingletonBase Instance
+        {
+            get
+            {
+                lock (_mutex)
+                {
+                    if (null == _instance)
+                    {
+                        _instance = new SingletonBase();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+
+    public abstract class Singleton<T> where T : class, new()
+    {
+        private static T _instance;
+        private static readonly object _mutex = new object();
+
+        protected Singleton()
+        {
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                lock (_mutex)
+                {
+                    if (null == _instance)
+                    {
+                        _instance = new T();
+                    }
+                    return _instance;
+                }
+            }
+        }
+    }
+}
